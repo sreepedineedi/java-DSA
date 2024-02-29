7c24df067877cf34a26c201efa8348d1e2b2ea8d
sreepedineedi <pedineedisree@gmail.com>
2024-02-29 20:01:22 +0530

    SI is added

---
+++ hello world/src/SimpleIntrest.java
@@ -0,0 +1,10 @@
+public class SimpleIntrest {
+    public static void main(String args[])
+    {
+        int p = 40;
+        int t = 30;
+        int r = 70;
+        int SI = (p*t*r)/100;
+        System.out.println("SI is "+SI);
+    }
+}
