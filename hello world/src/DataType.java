cbb028af7dc88b3163de282eb6beb28452085839
sreepedineedi <pedineedisree@gmail.com>
2024-03-13 20:34:33 +0530

    data types added

---
+++ hello world/src/DataType.java
@@ -0,0 +1,23 @@
+import java.util.function.LongToIntFunction;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        System.out.println("minimum value of" +Double.MIN_VALUE);
+        System.out.println("maximum value of" +Double.MAX_VALUE);
+        System.out.println("minimum value of" +Long.MIN_VALUE);
+        System.out.println("maximum value of" +Long.MAX_VALUE);
+        System.out.println("minimum value of" +Boolean.TYPE);
+        System.out.println("minimum value of" +Float.MIN_VALUE);
+        System.out.println("maximum value of" +Float.MAX_VALUE);
+        System.out.println("size of integer " +Integer.SIZE + "or" +Integer.SIZE/8 );
+        System.out.println("size of double " +Double.SIZE + "or" +Double.SIZE/8);
+        System.out.println("size of long" +Long.SIZE + "or" +Long.SIZE/8);
+        System.out.println("size of float" +Float.SIZE + "or" +Float.SIZE/8);
+
+
+
+
+
+    }
+}
