dad052ee591647eaf310fa2daba134744ab4672a
sreepedineedi <pedineedisree@gmail.com>
2024-03-16 17:37:19 +0530

    perimeter of circle

--- hello world/src/PerimeterOfCircle.java
+++ hello world/src/PerimeterOfCircle.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class PerimeterOfCircle {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("perimeter is");
+        float radius = input.nextFloat();
+        double perimeter = (3.14*radius);
+        System.out.println("perimeter is" +perimeter);
 
     }
 
